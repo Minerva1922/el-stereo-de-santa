9d7016a7395e5319fb8c177bff5bc27fbd4773da
Minerva1922 <minervapv83@gmail.com>
2023-11-22 14:30:02 +0100

    estilos inicio

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import "../app.css";
+</script>
+
+<main>
+    <slot />
+</main>
+
+<section>
+    <h1>El Stereo de Santa</h1>
+</section>
+
+<style>
+     h1 {
+         color: #CC1C1C;
+         font-size: 40px;
+         text-align: center;
+     }
+
+     section {
+         background-color: #FEF9E2;
+         width: 324px;
+         height: 778px;
+         margin-top: 67px;
+         display: flex;
+         margin-left: 48px;
+         border-radius: 25px;
+         border: 5px solid #CC1C1C; /* Cambia #E57373 por el color de borde que deseas */
+     }
+
+</style>
